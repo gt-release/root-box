55ecc3be062fea61715ebb66e980a92677df7976
takawagu <gt.release1@gmail.com>
2019-05-27 00:34:42 +0900

    fix redis config

--- src/main/kotlin/jp/co/takawagu/rootBox/config/RedisConfig.kt
+++ src/main/kotlin/jp/co/takawagu/rootBox/config/RedisConfig.kt
@@ -1,14 +1,20 @@
 package jp.co.takawagu.rootBox.config
 
+import io.lettuce.core.RedisURI
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 
 @Configuration
 class RedisConfig {
 
     @Bean
-    fun jedisConnectionFactory(): JedisConnectionFactory {
-        return JedisConnectionFactory()
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        val uri = RedisURI.create(System.getenv("REDIS_URL"))
+        val redisConfig = RedisStandaloneConfiguration(uri.host, uri.port)
+        redisConfig.password = RedisPassword.of(uri.password)
+        return LettuceConnectionFactory(redisConfig)
     }
 }
